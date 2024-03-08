cb5b36f2942b2ce41643af9ca6b5a3e6a240826f
rakototsiferason <andrana.anthony@gmail.com>
2024-03-08 20:49:10 +0300

    backup DL-API.yml

---
+++ Memoire INSI/utils.py
@@ -0,0 +1,10 @@
+from datetime import timedelta
+from random import randrange
+
+
+def random_date(start, end):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+
+    return start + timedelta(seconds=random_second)
